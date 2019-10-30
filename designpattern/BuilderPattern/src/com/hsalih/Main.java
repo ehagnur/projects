f848c3201d190ac60b9a4e8136b780219bb6b5bc
Hagos Salih <hsalih@box.com>
2019-10-30 15:42:48 -0700

    removed personal info

--- designpattern/BuilderPattern/src/com/hsalih/Main.java
+++ designpattern/BuilderPattern/src/com/hsalih/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         User test_user1 = new User.Builder("user123", "user123@company.com").build();
-        User test_user2 = new User.Builder("user245", "user245@anothercompany.com").firstName("bob").lastName("Mayer").phoneNumber(408900800).address("1700 Park Ave, San Jose,CA").build();
+        User test_user2 = new User.Builder("user245", "user245@anothercompany.com").firstName("first_name").lastName("last_name").phoneNumber(1234567890).address("1234 main st., City,ST").build();
         System.out.println(test_user1);
         System.out.println(test_user2);
     }
